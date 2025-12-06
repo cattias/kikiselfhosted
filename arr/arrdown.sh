982a28634134b9badc37fd3b179b7d1277bf9ce5
Christophe Attias <christophe.attias@gmail.com>
2025-12-06 11:58:04 +0100

    bazarr

--- arr/arrdown.sh
+++ arr/arrdown.sh
@@ -2,6 +2,7 @@
 kubectl scale deployment -n arr prowlarr --replicas=0
 kubectl scale deployment -n arr lidarr --replicas=0
 kubectl scale deployment -n arr radarr --replicas=0
+kubectl scale deployment -n arr bazarr --replicas=0
 kubectl scale deployment -n arr sonarr --replicas=0
 kubectl scale deployment -n arr metube --replicas=0
 kubectl scale deployment -n arr sabnzbd --replicas=0
