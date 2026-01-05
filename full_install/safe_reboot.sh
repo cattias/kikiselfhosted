5d96600dc95c314c7ab643ddb86d70294066b5d2
Christophe Attias <christophe.attias@gmail.com>
2026-01-05 12:57:50 +0100

    safe reboot script

---
+++ full_install/safe_reboot.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "🚀 Starting Graceful Reboot..."
+
+# 2. Stop ArgoCD Controller
+echo "⏸️ Scaling down ArgoCD..."
+kubectl -n argocd scale statefulset/argocd-application-controller --replicas=0
+
+# 3. Drain the Node (Forcing past PDBs)
+echo "🧹 Draining kikiserver ..."
+# --disable-eviction is the key to bypassing those Paperless/Immich errors
+kubectl drain kikiserver --ignore-daemonsets --delete-emptydir-data --force --grace-period=30 --disable-eviction
+# For pods suck in Terminating
+kubectl get pods -A | grep Terminating | awk '{print "kubectl delete pod " $2 " -n " $1 " --force --grace-period=0"}' | sh
+
+# 4. Stop K0s engine
+echo "🛑 Stopping k0s..."
+sudo k0s stop
+
+# 5. Flush NFS
+echo "💾 Unmounting NFS..."
+# sync
+sudo umount -fl /mnt/ugreen-media
+sudo umount -fl /mnt/ugreen-backup
+sudo modprobe -r ceph
+sudo modprobe -r libceph
+
+echo "♻️ Rebooting now..."
+sudo reboot
+
+# after reboot
+kubectl uncordon kikiserver
+kubectl -n argocd scale statefulset/argocd-application-controller --replicas=1
