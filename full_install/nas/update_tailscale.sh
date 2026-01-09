4f9c0d0ef37eead0af8ba85d6328191bbbde6e94
Christophe Attias <christophe.attias@gmail.com>
2026-01-09 17:21:44 +0100

    tailscale vps

--- full_install/nas/update_tailscale.sh
+++ full_install/nas/update_tailscale.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
-wget https://pkgs.tailscale.com/stable/tailscale_1.92.5_amd64.tgz
-tar xzvf tailscale_1.92.5_amd64.tgz
+#!/bin/bash
+TAILSCALE_VERSION=1.92.5
+
+wget https://pkgs.tailscale.com/stable/tailscale_${TAILSCALE_VERSION}_amd64.tgz
+tar xzvf tailscale_${TAILSCALE_VERSION}_amd64.tgz
+cd tailscale_${TAILSCALE_VERSION}_amd64
+sudo tailscale down
 sudo systemctl stop tailscaled
-sudo cp tailscaled /usr/sbin/tailscaled 
-sudo cp tailscale /usr/local/bin/tailscale
+sudo cp tailscale /usr/local/bin/
+sudo cp tailscaled /usr/sbin/
 sudo systemctl start tailscaled
+sudo tailscale up
