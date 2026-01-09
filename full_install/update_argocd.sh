1afc9d50fcee62c81b341abfa06f95d9ff388753
Christophe Attias <christophe.attias@gmail.com>
2026-01-09 22:58:13 +0100

    argocd vault plugin

--- full_install/update_argocd.sh
+++ full_install/update_argocd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ARGOCD_LATEST=$(curl -sSL https://api.github.com/repos/argoproj/argo-cd/releases/latest | grep '"tag_name":' | head -1 | awk -F '"' '{print $4}' | tr -d 'v')
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v$ARGOCD_LATEST/manifests/install.yaml
-kubectl patch deployment argocd-repo-server -n argocd --patch-file argocd-repo-server-deployment.yaml
+# kubectl patch deployment argocd-repo-server -n argocd --patch-file argocd-repo-server-deployment.yaml
 kubectl apply -f ../apps/templates/argocd-cmd-params-cm.yaml
 ARGOCD_SERVER=$(kubectl get pod -n argocd -l app.kubernetes.io/name=argocd-server -o jsonpath='{.items[0].metadata.name}')
 kubectl delete po -n argocd $ARGOCD_SERVER
