0c7f76115fbb495c2ea33fa685c2e1b32af707b7
Christophe Attias <christophe.attias@gmail.com>
2026-01-13 16:24:24 +0100

    arr pv

--- arr/arrdown.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-kubectl scale deployment -n arr prowlarr --replicas=0
-kubectl scale deployment -n arr lidarr --replicas=0
-kubectl scale deployment -n arr radarr --replicas=0
-kubectl scale deployment -n arr bazarr --replicas=0
-kubectl scale deployment -n arr sonarr --replicas=0
-kubectl scale deployment -n arr metube --replicas=0
-kubectl scale deployment -n arr sabnzbd --replicas=0
