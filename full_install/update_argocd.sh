e5d391c500680b6d2fb47512e976baa48e02a07f
Christophe Attias <christophe.attias@gmail.com>
2026-02-02 10:34:30 +0100

    update argocd script

--- full_install/update_argocd.sh
+++ full_install/update_argocd.sh
@@ -3,8 +3,9 @@ ARGOCD_LATEST=$(curl -sSL https://api.github.com/repos/argoproj/argo-cd/releases
 kubectl apply --server-side --force-conflicts -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v$ARGOCD_LATEST/manifests/install.yaml
 # kubectl patch deployment argocd-repo-server -n argocd --patch-file argocd-repo-server-deployment.yaml
 kubectl patch configmap argocd-cmd-params-cm -n argocd --type merge -p '{"data":{"server.insecure":"true"}}'
-ARGOCD_SERVER=$(kubectl get pod -n argocd -l app.kubernetes.io/name=argocd-server -o jsonpath='{.items[0].metadata.name}')
-kubectl delete po -n argocd $ARGOCD_SERVER
+
+kubectl rollout restart deployment -n argocd argocd-server
+kubectl rollout restart deployment -n argocd argocd-dex-server
 
 # On the mac - update the client
 # brew upgrade argocd
