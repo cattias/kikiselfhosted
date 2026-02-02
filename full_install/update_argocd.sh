2dd44a4b71bd85a83dca024dfdca81c1cd8d87e6
Christophe Attias <christophe.attias@gmail.com>
2026-02-02 10:09:15 +0100

    update argocd script

--- full_install/update_argocd.sh
+++ full_install/update_argocd.sh
@@ -2,11 +2,9 @@
 ARGOCD_LATEST=$(curl -sSL https://api.github.com/repos/argoproj/argo-cd/releases/latest | grep '"tag_name":' | head -1 | awk -F '"' '{print $4}' | tr -d 'v')
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v$ARGOCD_LATEST/manifests/install.yaml
 # kubectl patch deployment argocd-repo-server -n argocd --patch-file argocd-repo-server-deployment.yaml
-kubectl apply -f ../apps/templates/argocd-cmd-params-cm.yaml
+kubectl patch configmap argocd-cmd-params-cm -n argocd --type merge -p '{"data":{"server.insecure":"true"}}'
 ARGOCD_SERVER=$(kubectl get pod -n argocd -l app.kubernetes.io/name=argocd-server -o jsonpath='{.items[0].metadata.name}')
 kubectl delete po -n argocd $ARGOCD_SERVER
 
-brew upgrade argocd
-# wget https://github.com/argoproj/argo-cd/releases/download/v$ARGOCD_LATEST/argocd-linux-amd64 -o argocd
-# chmod a+x argocd
-# sudo mv argocd /usr/local/bin/argocd
+# On the mac - update the client
+# brew upgrade argocd
