ec4f88fdb446627522c7cf868a148017f32782b8
Christophe Attias <github@attias.io>
2026-02-18 23:04:31 +0100

    soulsolid 0.20.4

--- arr/deploy-new-soulsolid.sh
+++ arr/deploy-new-soulsolid.sh
@@ -3,6 +3,6 @@ sudo docker buildx build \
   --platform linux/amd64 \
   -t kikiattias/soulsolid-plugins:latest \
   --push .
-kubectl config use-context Server
+# kubectl config use-context Server
 kubectl rollout restart deployment soulsolid -n arr
 
